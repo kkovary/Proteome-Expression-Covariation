95de0923c154936b8564904d08b92f184cbc0278
Kyle <kylekovary@protonmail.com>
2020-03-10 19:09:10 -0700

    .

--- functions.R
+++ functions.R
@@ -18,10 +18,8 @@ corProts <- function(nProt, nCell, R, mean, sd) {
   cor_matrix =  matrix(data = R,
                        ncol = nProt,
                        nrow = nProt)
-  for (i in 1:nProt) {
-    cor_matrix[i, i] = 1
-  }
   
+  diag(cor_matrix) <- 1
   list_distributions <- list()
   for (i in 1:nProt) {
     list_distributions <-
@@ -31,4 +29,44 @@ corProts <- function(nProt, nCell, R, mean, sd) {
   
   mat = fun(cor_matrix, list_distributions, nCell)
   return(mat)
+}
+
+# Rug Plots
+cor_rug_plot <- function(x, color = '#e08214'){
+  temp <- filter(allGroups, accession %in% x) %>%
+    dplyr::select(name, cors) %>% unnest(cols = cors)
+  
+  temp <- allGroups %>% select(name, cors) %>%
+    unnest(cols = cors) %>% mutate(name = "all") %>%
+    rbind(temp,.)
+  
+  p1 <- ggplot(temp, aes(x = cors, fill = name)) + geom_density(alpha = 0.75, color = "transparent") +
+    scale_fill_manual(values = c('#bababa',color), name = "") +
+    geom_rug(data = filter(temp, name != "all"), color = color) +
+    xlab("") + ylab("Density") +
+    theme_bw() +
+    # theme(legend.position = c(.2,.9),
+    #       legend.background = element_rect(fill="transparent")) +
+    theme(legend.position = "none",
+          text = element_text(size = 6)) +
+    xlim(-1,1) +
+    ggtitle(filter(temp, name != "all") %>% pull(name) %>% unique())
+  
+  p2 <- ggplot(temp, aes(x = name, y = cors, fill = name)) + 
+    geom_boxplot(notch = TRUE, alpha = 0.75) +
+    scale_fill_manual(values = c('#bababa',color)) +
+    ylab("Correlation Coefficients") +
+    theme_bw() +
+    theme(legend.position = "none",
+          text = element_text(size = 6),
+          axis.title.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank()) +
+    ylim(-1,1) +
+    coord_flip()
+  
+  # p1/p2 + 
+  #   #plot_annotation(tag_levels = "A") +
+  #   plot_layout(heights = c(2,1))
+  plot_grid(p1, p2, ncol = 1, rel_heights = c(2,1), align = "v")
 }
